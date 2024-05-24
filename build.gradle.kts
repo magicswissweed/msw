b1e7c8841292fe560fb4cd905bea09974e67cac1
Aaron Studer <aaron.studer@ergon.ch>
2024-05-24 10:16:03 +0200

    add flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.0"
 	id("io.spring.dependency-management") version "1.1.5"
+	id("org.flywaydb.flyway") version "9.7.0"
 }
 
 group = "com.aa"
@@ -18,11 +19,22 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-jooq")
+	implementation("org.flywaydb:flyway-core:9.8.2")
+
 	runtimeOnly("org.postgresql:postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+flyway {
+	url = "jdbc:postgresql://localhost:7010/gradlecodegen"
+	user = "gradle"
+	password = "password"
+	locations = listOf("filesystem:" + project.rootDir + "/src/main/resources/db/migration").toTypedArray()
 }
---
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE sample_table
+(
+    id                      UUID PRIMARY KEY,
+    timestamp               TIMESTAMP WITH TIME ZONE,
+    temperature             REAL,
+    flow                    INTEGER
+)
