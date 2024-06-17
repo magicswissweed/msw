301c781652e862afaee1a5d568fa18a726abb066
Aaron Studer <aaron.studer@ergon.ch>
2024-06-17 10:31:59 +0200

    style login form

--- frontend/src/user/login/MswLogin.jsx
+++ frontend/src/user/login/MswLogin.jsx
@@ -35,45 +35,49 @@ export const MswLogin = () => {
 
     return (
         <>
-            <div className="p-4 box">
-                <h2 className="mb-3">Firebase/ React Auth Login</h2>
+            <div className="form">
+                <div className="box-container">
+                    <div className="p-4 box">
+                        <h2 className="mb-3">Login</h2>
 
-                {error && <Alert variant="danger">{error}</Alert>}
+                        {error && <Alert variant="danger">{error}</Alert>}
 
-                <Form onSubmit={handleSubmit}>
+                        <Form onSubmit={handleSubmit}>
 
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control
-                            type="email"
-                            placeholder="Email address"
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Email address"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Control
-                            type="password"
-                            placeholder="Password"
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </Form.Group>
 
-                    <div className="d-grid gap-2">
-                        <Button variant="primary" type="Submit">
-                            Log In
-                        </Button>
+                            <div className="d-grid gap-2">
+                                <Button variant="primary" type="Submit">
+                                    Log In
+                                </Button>
+                            </div>
+                        </Form>
+                        <hr/>
+                        {/*<div className="google-button-container">*/}
+                        {/*    <GoogleButton*/}
+                        {/*        type="dark"*/}
+                        {/*        onClick={handleGoogleSignIn}*/}
+                        {/*    />*/}
+                        {/*</div>*/}
                     </div>
-                </Form>
-                <hr/>
-                {/*<div className="google-button-container">*/}
-                {/*    <GoogleButton*/}
-                {/*        type="dark"*/}
-                {/*        onClick={handleGoogleSignIn}*/}
-                {/*    />*/}
-                {/*</div>*/}
-            </div>
-            <div className="p-4 box mt-3 text-center">
-                Don't have an account? <Link to="/signup">Sign up</Link>
+                    <div className="p-4 box mt-3 text-center">
+                        Don't have an account? <Link to="/signup">Sign up</Link>
+                    </div>
+                </div>
             </div>
         </>
     );
--- frontend/src/user/signup/MswSignUp.jsx
+++ frontend/src/user/signup/MswSignUp.jsx
@@ -24,38 +24,42 @@ const MswSignup = () => {
 
     return (
         <>
-            <div className="p-4 box">
-                <h2 className="mb-3">Firebase/ React Auth Signup</h2>
+            <div className="form">
+                <div className="box-container">
+                    <div className="p-4 box">
+                        <h2 className="mb-3">Signup</h2>
 
-                {error && <Alert variant="danger">{error}</Alert>}
+                        {error && <Alert variant="danger">{error}</Alert>}
 
-                <Form onSubmit={handleSubmit}>
+                        <Form onSubmit={handleSubmit}>
 
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control
-                            type="email"
-                            placeholder="Email address"
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Email address"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Control
-                            type="password"
-                            placeholder="Password"
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </Form.Group>
 
-                    <div className="d-grid gap-2">
-                        <Button variant="primary" type="Submit">
-                            Sign up
-                        </Button>
+                            <div className="d-grid gap-2">
+                                <Button variant="primary" type="Submit">
+                                    Sign up
+                                </Button>
+                            </div>
+                        </Form>
                     </div>
-                </Form>
-            </div>
-            <div className="p-4 box mt-3 text-center">
-                Already have an account? <Link to="/">Log In</Link>
+                    <div className="p-4 box mt-3 text-center">
+                        Already have an account? <Link to="/login">Log In</Link>
+                    </div>
+                </div>
             </div>
         </>
     );
