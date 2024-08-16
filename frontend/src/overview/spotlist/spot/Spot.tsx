c2cdad527bdd39634a797979383613c4b0f274a0
Aaron Studer <aaron.studer@ergon.ch>
2024-08-16 14:35:30 +0200

    Add Delete-Spot-Button for mobile

--- frontend/src/overview/spotlist/spot/Spot.tsx
+++ frontend/src/overview/spotlist/spot/Spot.tsx
@@ -6,6 +6,7 @@ import {MswMiniGraph} from './miniForecast/MswMiniGraph';
 import {MswForecastGraph} from './forecast/MswForecastGraph';
 import arrow_down from '../../../assets/arrow_down.png';
 import lock from '../../../assets/lock.svg';
+import delete_icon from '../../../assets/delete_icon.svg';
 import {MswLastMeasurementsGraph} from './historical/MswLastMeasurementsGraph';
 import {authConfiguration} from '../../../api/config/AuthConfiguration';
 import {useUserAuth} from '../../../user/UserAuthContext';
@@ -40,14 +41,16 @@ export const Spot = (props: SpotProps) => {
                 <MswMiniGraph location={location}/>
             </div>
             <div className="icons-container">
-                <div className="is-private-icon icon">
+                <div className="icon">
                     <img className={location.isPublic ? "public" : ""}
                          alt="This is a private spot. Only you can see it."
                          title="This is a private spot. Only you can see it."
                          src={lock}/>
                 </div>
-                <div className="icon"></div>
-                <div className="collapsibleIcon icon">
+                <div className="icon" onClick={() => onDeleteSpot(location)}>
+                    <img alt="Delete this spot from your dashboard." src={delete_icon}/>
+                </div>
+                <div className="collapsible-icon icon">
                     <img alt="extend forecast" src={arrow_down}/>
                 </div>
             </div>
@@ -65,13 +68,9 @@ export const Spot = (props: SpotProps) => {
             <MswLastMeasurementsGraph location={location} isMini={false}/>
         </>;
 
-        const privateSpotInteractions = <>
-            <button className="msw-button delete-spot-btn" onClick={() => onDeleteSpot(location)}>Delete Spot</button>
-        </>;
         return <>
             <div className="collapsibleContent">
                 {location.forecast ? forecastContent : lastMeasurementsContent}
-                {!location.isPublic && privateSpotInteractions}
             </div>
         </>;
     }
