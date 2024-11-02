24b5939c4c2e1e1a466e92593541f73537c06501
Nicola Küng <32466285+nkueng@users.noreply.github.com>
2024-11-02 19:06:00 +0100

    color flow and temp depending on values

--- frontend/src/overview/spotlist/spot/measurement/MswMeasurement.tsx
+++ frontend/src/overview/spotlist/spot/measurement/MswMeasurement.tsx
@@ -25,9 +25,8 @@ export class MswMeasurement extends Component<MeasurementsProps> {
                 </div>
 
                 {this.location.currentSample!.temperature &&
-                    <div className={"measurement_row meas temp"}>
-                        <div>{this.location.currentSample!.temperature}</div>
-                        <div className="unit">°C</div>
+                    <div className="measurement_row meas temp">
+                      {this.getTemp()}
                     </div>
                 }
             </div>
@@ -35,8 +34,9 @@ export class MswMeasurement extends Component<MeasurementsProps> {
     }
 
     private getFlow() {
+        let flow: number = this.location.currentSample!.flow;
         return <>
-            <div>{this.location.currentSample!.flow}</div>
+            <div className={this.getFlowColor(flow)}>{flow}</div>
             <div className="unit">
                 m<sup>3</sup>/s
             </div>
@@ -51,6 +51,44 @@ export class MswMeasurement extends Component<MeasurementsProps> {
             {/*  </div>*/}
             {/*)}*/}
         </>;
+    }
+
+    private getFlowColor(_flow: number) {
+
+        if (_flow < this.location.minFlow) {
+          return "flow_bad";
+        }
+
+        if (_flow < this.location.maxFlow) {
+          return "flow_good";
+        }
+
+        return "flow_bad";
+    }
+
+    private getTemp() {
+        let temp: number = this.location.currentSample!.temperature ?? 0;
+        return <>
+              <div className={this.getTempColor(temp)}>{temp}</div>
+              <div className="unit">°C</div>
+        </>;
+      }
+
+    private getTempColor(_temp: number) {
+      if (_temp < 8) {
+        return "temp_0";
+      }
+      if (_temp < 13) {
+        return "temp_1";
+      }
+      if (_temp < 18) {
+        return "temp_2";
+      }
+      if (_temp < 23) {
+        return "temp_3";
+      }
+      return "temp_4";
+    
     }
 
     private convertTimeStampToDisplayableString(timestampString: string) {
