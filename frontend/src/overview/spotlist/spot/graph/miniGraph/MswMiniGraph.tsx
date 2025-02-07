f58fd7be90a178c95d28b245afe1f52d43d98e84
aastuder <aaron.studer@ergon.ch>
2025-02-07 10:15:37 +0100

    One radio button for all graphs - Done: - Only one toggle button for all spots instead of one for each. - Toggles are too big on mobile. - Also toggle miniForecast
    - TODO:
    - Ticks are not perfect yet.
    - Style the Radio buttons?

--- frontend/src/overview/spotlist/spot/graph/miniGraph/MswMiniGraph.tsx
+++ frontend/src/overview/spotlist/spot/graph/miniGraph/MswMiniGraph.tsx
@@ -1,36 +1,36 @@
 import './MswMiniGraph.scss'
-import React, {Component} from 'react';
+import React from 'react';
 import {ApiSpotInformation} from '../../../../../gen/msw-api-ts';
 import {MswForecastGraph} from '../forecast/MswForecastGraph';
 import {MswLastMeasurementsGraph} from '../historical/MswLastMeasurementsGraph';
+import {GraphTypeEnum} from "../../../../MswOverviewPage";
+import {MswHistoricalYearsGraph} from "../historical/MswHistoricalYearsGraph";
 
 interface MswMiniForecastProps {
-    location: ApiSpotInformation
+    location: ApiSpotInformation,
+    showGraphOfType: GraphTypeEnum
 }
 
-export class MswMiniGraph extends Component<MswMiniForecastProps> {
-    private readonly location: ApiSpotInformation;
+export const MswMiniGraph = (props: MswMiniForecastProps) => {
 
-    constructor(props: MswMiniForecastProps) {
-        super(props);
-        this.location = props.location;
-    }
-
-    render() {
-        let content;
-        if (this.location.forecast) {
-            content = <MswForecastGraph location={this.location} aspectRatio={3}/>;
+    let content;
+    if(props.showGraphOfType === GraphTypeEnum.Forecast) {
+        if (props.location.forecast) {
+            content = <MswForecastGraph location={props.location} aspectRatio={3}/>;
         } else {
-            content = <MswLastMeasurementsGraph location={this.location} aspectRatio={3}/>
+            content = <MswLastMeasurementsGraph location={props.location} aspectRatio={3}/>
         }
-
-        let className = "forecastContainer";
-        return <>
-            <div className={className}>
-                <div className="miniGraph">
-                    {content}
-                </div>
-            </div>
-        </>;
+    } else {
+        content = <MswHistoricalYearsGraph location={props.location} aspectRatio={3} />
     }
+
+
+    let className = "forecastContainer";
+    return <>
+        <div className={className}>
+            <div className="miniGraph">
+                {content}
+            </div>
+        </div>
+    </>;
 }
