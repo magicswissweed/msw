deabdfb0d7a125550d302389edba4e10503b2ade
nkueng <nkueng@ethz.ch>
2025-04-21 19:55:08 +0200

    make sure not to mistake Wasserstand for Abfluss data

--- backend/src/main/java/com/aa/msw/source/hydrodaten/historical/lastfourty/Last40DaysSampleFetchService.java
+++ backend/src/main/java/com/aa/msw/source/hydrodaten/historical/lastfourty/Last40DaysSampleFetchService.java
@@ -1,15 +1,19 @@
 package com.aa.msw.source.hydrodaten.historical.lastfourty;
 
-import com.aa.msw.database.helpers.id.SampleId;
-import com.aa.msw.model.Sample;
+import com.aa.msw.database.helpers.id.Last40DaysId;
+import com.aa.msw.model.Last40Days;
 import com.aa.msw.source.hydrodaten.AbstractLineFetchService;
+import com.aa.msw.source.hydrodaten.model.line.HydroLine;
 import com.aa.msw.source.hydrodaten.model.line.HydroResponse;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.OffsetDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 @Service
 public class Last40DaysSampleFetchService extends AbstractLineFetchService {
@@ -18,30 +22,35 @@ public class Last40DaysSampleFetchService extends AbstractLineFetchService {
         super("https://www.hydrodaten.admin.ch/plots/p_q_40days/", "_p_q_40days_de.json");
     }
 
-    private List<Sample> fetchLast40DaysSamples(int stationId) throws IOException, URISyntaxException {
+    private Last40Days fetchLast40DaysSamples(int stationId) throws IOException, URISyntaxException {
         HydroResponse hydroResponse = fetchFromHydro(stationId);
+        Map<OffsetDateTime, Double> line;
+        // Check for flow measurement
+        ArrayList<HydroLine> data = hydroResponse.plot().data();
+        if (data.size() < 2) {
+            if (!data.get(0).name().equals("Abfluss")) {
+                throw new IOException("Flow measurement not available");
+            }
+            line = mapLine(data.get(0));
 
-        Map<OffsetDateTime, Double> line = mapLine(hydroResponse.plot().data().get(1));
+        } else {
+            line = mapLine(data.get(1));
+        }
 
-        return line.entrySet().stream()
-                .filter(entry -> entry.getValue() != null)
-                .map(entry -> new Sample(
-                        new SampleId(),
-                        stationId,
-                        entry.getKey(),
-                        Optional.empty(),
-                        entry.getValue().intValue()
-                ))
-                .toList();
+        return new Last40Days(
+                new Last40DaysId(),
+                stationId,
+                line
+        );
     }
 
-    public Set<List<Sample>> fetchLast40DaysSamples(Set<Integer> stationIds) throws URISyntaxException {
-        Set<List<Sample>> result = new HashSet<>();
+    public Set<Last40Days> fetchLast40DaysSamples(Set<Integer> stationIds) throws URISyntaxException {
+        Set<Last40Days> result = new HashSet<>();
         for (Integer stationId : stationIds) {
             try {
                 result.add(fetchLast40DaysSamples(stationId));
             } catch (IOException e) {
-                // ignore, there might not be last 40 days data
+                // ignore, there might not be data from last 40 days
             }
         }
         return result;
