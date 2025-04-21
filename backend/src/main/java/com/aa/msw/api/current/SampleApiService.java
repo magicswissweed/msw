142a97e18b4d79c2cb75efd68c29fc9ba0105f74
nkueng <nkueng@ethz.ch>
2025-04-21 20:05:44 +0200

    bring back API method to get all last 40 days samples

--- backend/src/main/java/com/aa/msw/api/current/SampleApiService.java
+++ backend/src/main/java/com/aa/msw/api/current/SampleApiService.java
@@ -1,27 +1,28 @@
 package com.aa.msw.api.current;
 
+import com.aa.msw.api.graph.last40days.Last40daysApiService;
 import com.aa.msw.database.exceptions.NoDataAvailableException;
 import com.aa.msw.database.repository.dao.SampleDao;
 import com.aa.msw.gen.api.ApiSample;
 import com.aa.msw.model.Sample;
 import com.aa.msw.source.InputDataFetcherService;
-import com.aa.msw.source.hydrodaten.historical.lastfourty.Last40DaysSampleFetchService;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.List;
 
 @Service
 public class SampleApiService {
 
     private final SampleDao sampleDao;
     private final InputDataFetcherService inputDataFetcherService;
-    private final Last40DaysSampleFetchService last40DaysSampleFetchService;
+    private final Last40daysApiService last40daysApiService;
 
-    SampleApiService(final SampleDao sampleDao, InputDataFetcherService inputDataFetcherService, Last40DaysSampleFetchService last40DaysSampleFetchService) {
+    SampleApiService(final SampleDao sampleDao, InputDataFetcherService inputDataFetcherService, Last40daysApiService last40daysApiService) {
         this.sampleDao = sampleDao;
         this.inputDataFetcherService = inputDataFetcherService;
-        this.last40DaysSampleFetchService = last40DaysSampleFetchService;
+        this.last40daysApiService = last40daysApiService;
     }
 
     private static ApiSample mapSample(Sample sample) {
@@ -42,5 +43,16 @@ public class SampleApiService {
             // NOP
         }
 
+    }
+
+    public List<ApiSample> getLast40DaysSamples(Integer stationId) throws IOException, URISyntaxException {
+        return last40daysApiService.getLast40Days(stationId).last40DaysSamples()
+                .entrySet()
+                .stream()
+                .map(sample -> new ApiSample()
+                        .timestamp(sample.getKey())
+                        // TODO: use double for flow?
+                        .flow(sample.getValue().intValue()))
+                .toList();
     }
 }
