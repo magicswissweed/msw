52d769953ea635345ccf5ff7c8e71254b7495f97
nkueng <nkueng@ethz.ch>
2025-05-16 12:17:43 +0000

    Add "About" page with "Buy me a coffee" button

--- frontend/src/footer/MswFooter.tsx
+++ frontend/src/footer/MswFooter.tsx
@@ -1,9 +1,13 @@
-import React, {Component} from 'react';
-import './MswFooter.scss'
+import React, {useState} from 'react';
+import './MswFooter.scss';
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
-export class MswFooter extends Component {
-    render() {
-        return <>
+export const MswFooter = () => {
+    const [showAboutModal, setShowAboutModal] = useState(false);
+
+    return (
+        <>
             <footer>
                 <div className="Footer">
                     <ul className='Footer_list'>
@@ -14,13 +18,56 @@ export class MswFooter extends Component {
                         </li>
                         <li>Created by Nicola K&#252;ng and Aaron Studer</li>
                         <li>
-                            <a href="mailto:magicswissweed@gmail.com?subject=Feedback">
-                                Feedback
-                            </a>
+                            <Button variant="link" onClick={() => setShowAboutModal(true)}>
+                                About
+                            </Button>
                         </li>
                     </ul>
                 </div>
             </footer>
-        </>;
-    }
-}
+
+            <Modal show={showAboutModal} onHide={() => setShowAboutModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>About</Modal.Title>
+                </Modal.Header>
+                <Modal.Body style={{textAlign: "center"}}>
+                    <p>
+                        This website is a passion project by Aaron Studer and Nicola Küng.
+                    </p>
+                    <p>
+                        Nicola built a <a href={"https://nkueng.github.io/MagicSwissWeed/"}>first version</a> of
+                        MagicSwissWeed to learn JavaScript and bring public data to life.
+                    </p>
+                    <p>
+                        When he met Aaron over a river surf session, they dreamed up grand ideas for an improved second
+                        version.
+                    </p>
+                    <p>
+                        Thanks to Aaron‘s programming skills, these dreams actually took shape in what you have before
+                        you.
+                    </p>
+                    <p>
+                        If you find this site useful, we would appreciate your support and feedback.
+                    </p>
+
+                </Modal.Body>
+                <Modal.Footer style={{justifyContent: "center"}}>
+                    <div style={{display: "flex", gap: "1rem"}}>
+                        <a href="https://www.buymeacoffee.com/magicswissweed" target="_blank" rel="noopener noreferrer">
+                            <img
+                                src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+                                alt="Buy Me A Coffee"
+                                style={{height: "38px"}}
+                            />
+                        </a>
+                        <Button variant="msw"
+                                style={{width: "135px"}}
+                                onClick={() => window.location.href = "mailto:magicswissweed@gmail.com?subject=Feedback"}>
+                            Feedback
+                        </Button>
+                    </div>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+};
