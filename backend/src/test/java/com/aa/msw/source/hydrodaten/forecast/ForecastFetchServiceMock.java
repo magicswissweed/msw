1110192927dda8d366cf476e0e076d8360e722c3
Aaron Studer <aaron.studer@ergon.ch>
2025-06-01 16:57:19 +0200

    add some data to ForecastFetchServiceMock

--- backend/src/test/java/com/aa/msw/source/hydrodaten/forecast/ForecastFetchServiceMock.java
+++ backend/src/test/java/com/aa/msw/source/hydrodaten/forecast/ForecastFetchServiceMock.java
@@ -1,25 +1,60 @@
 package com.aa.msw.source.hydrodaten.forecast;
 
+import com.aa.msw.database.helpers.id.ForecastId;
 import com.aa.msw.model.Forecast;
+import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @Profile("test")
 @Service
 class ForecastFetchServiceMock implements ForecastFetchService {
 
+    private final List<Forecast> forecasts = List.of(
+            forecast(2018),
+            forecast(2243),
+            forecast(2174)
+    );
+
     @Override
-    public List<Forecast> fetchForecasts(Set<Integer> stationIds) throws URISyntaxException {
-        return List.of();
+    public List<Forecast> fetchForecasts(Set<Integer> stationIds) {
+        return forecasts;
+    }
+
+    private OffsetDateTime offsetDateTime(int year, int month, int dayOfMonth) {
+        return OffsetDateTime.of(year, month, dayOfMonth, 0, 0, 0, 0, ZoneOffset.UTC);
     }
 
     @Override
-    public Forecast fetchForecast(int stationId) throws IOException, URISyntaxException {
-        return null;
+    public Forecast fetchForecast(int stationId) {
+        for (Forecast forecast : forecasts) {
+            if (forecast.stationId() == stationId) {
+                return forecast;
+            }
+        }
+        throw new NotImplementedException("Add the forecast with this stationId to the ForecastFetchServiceMock");
+    }
+
+    private Forecast forecast(int stationId) {
+        return new Forecast(
+                new ForecastId(),
+                stationId,
+                offsetDateTime(2025, 1, 1),
+                Map.of(
+                        offsetDateTime(2024, 12, 31), 200.0,
+                        offsetDateTime(2025, 1, 1), 180.0
+                ),
+                Map.of(offsetDateTime(2025, 1, 2), 250.0),
+                Map.of(offsetDateTime(2025, 1, 3), 240.0),
+                Map.of(offsetDateTime(2025, 1, 2), 260.0),
+                Map.of(offsetDateTime(2025, 1, 2), 200.0),
+                Map.of(offsetDateTime(2025, 1, 2), 300.0)
+        );
     }
 }
