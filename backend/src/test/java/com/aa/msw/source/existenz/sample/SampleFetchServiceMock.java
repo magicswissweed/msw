0b3eeb116a8103aa4cd51893ec4bd5efd7676ed7
Aaron Studer <aaron.studer@ergon.ch>
2025-06-01 16:56:04 +0200

    add all samples of public spots to SampleFetchServiceMock

--- backend/src/test/java/com/aa/msw/source/existenz/sample/SampleFetchServiceMock.java
+++ backend/src/test/java/com/aa/msw/source/existenz/sample/SampleFetchServiceMock.java
@@ -20,7 +20,12 @@ class SampleFetchServiceMock implements SampleFetchService {
         return List.of(
                 sample(2018, Optional.of(15.0), 200),
                 sample(2243, Optional.of(14.0), 100),
-                sample(2105, Optional.empty(), 20)
+                sample(2105, Optional.empty(), 20),
+                sample(2152, Optional.of(13.0), 70),
+                sample(2091, Optional.of(12.0), 850),
+                sample(2135, Optional.of(11.0), 90),
+                sample(2473, Optional.of(10.0), 120),
+                sample(2152, Optional.of(9.0), 310)
         );
     }
 
